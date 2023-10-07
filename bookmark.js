55a02ef7a3680c0552d9b15596d57ae291cf5080
YtFanboiiiExe <139304491+YtFanboiiiExe@users.noreply.github.com>
2023-10-06 19:28:36 -0700

    Create bookmark.js

---
+++ bookmark.js
@@ -0,0 +1,5 @@
+javascript:(function() {
+    var script = document.createElement('script');
+    script.src = 'https://raw.githubusercontent.com/YtFanboiiiExe/IngotReborn/main/code/v1-0.js';
+    document.body.appendChild(script);
+})();
